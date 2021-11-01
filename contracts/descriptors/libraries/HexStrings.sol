d3e688b121f56ddfb52689afc0bf42442290b071
Wilson Cusack <wilson.cusack@coinbase.com>
2021-11-01 17:07:22 -0400

    added natspec for partialHexString, improved variable naming, made more generic

--- contracts/descriptors/libraries/HexStrings.sol
+++ contracts/descriptors/libraries/HexStrings.sol
@@ -3,14 +3,19 @@ pragma solidity 0.8.6;
 library HexStrings {
     bytes16 internal constant ALPHABET = '0123456789abcdef';
 
-    function partialHexString(uint160 value) internal pure returns (string memory) {
-        uint8 length = 3;
-        bytes memory buffer = new bytes(2 * length + 5);
+    // @notice returns value as a hex string of desiredPartialStringLength length,
+    // adding '0x' to the start and '...' to the end. 
+    // Designed to be used for shortening addresses for display purposes.
+    // @param value The value to return as a hex string
+    // @param desiredPartialStringLength How many hex characters of `value` to return in the string
+    // @param valueLengthAsHexString The length of `value` as a hex string
+    function partialHexString(uint160 value, uint8 desiredPartialStringLength, uint8 valueLengthAsHexString) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(desiredPartialStringLength + 5);
         buffer[0] = '0';
         buffer[1] = 'x';
-        uint8 offset = 2 * length + 1;
-        // remove values not in partial length
-        value >>= 4 * (40 - (length * 2));
+        uint8 offset = desiredPartialStringLength + 1;
+        // remove values not in partial length, four bytes for every hex character
+        value >>= 4 * (valueLengthAsHexString - desiredPartialStringLength);
         for (uint8 i = offset; i > 1; --i) {
             buffer[i] = ALPHABET[value & 0xf];
             value >>= 4;
