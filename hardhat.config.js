6f2763c18e8b25119686b4bc6cb364dff669edef
Wilson Cusack <wilson.cusack@coinbase.com>
2021-11-01 16:50:11 -0400

    added optimizer code

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,18 @@ const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
-  solidity: "0.8.6",
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+        details: {
+          yul: false
+        }
+      },
+    },
+  },
   gasReporter: {
     currency: 'USD',
     gasPrice: 38
