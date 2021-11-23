0e875ef9f3fb8a11f9eeef36afa609f665be511f
Wilson Cusack <cusackw@gmail.com>
2021-11-23 13:46:41 -0500

    added rinkeby and mainnet networks to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,7 +29,16 @@ module.exports = {
   networks: {
 	  hardhat: {
 	    chainId: 1337
-	  }
+	  },
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.RINKEBY_PRIVATE_KEY !== undefined ? [process.env.RINKEBY_PRIVATE_KEY] : [],
+    },
  }
 };
-
