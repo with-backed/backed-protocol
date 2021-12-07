3057c125cdac0f4ecc622a788fc893d1e7abccfa
Wilson Cusack <cusackw@gmail.com>
2021-12-07 10:45:09 -0500

    removed unused variables

--- contracts/descriptors/BorrowTicketDescriptor.sol
+++ contracts/descriptors/BorrowTicketDescriptor.sol
@@ -11,7 +11,7 @@ contract BorrowTicketDescriptor is NFTLoansTicketDescriptor {
      * @notice returns string with borrow ticket description details
      * @dev Called by generateDescriptor when populating the description part of the token metadata. 
      */
-    function generateDescription(string memory loanId) internal virtual override pure returns (string memory) {
+    function generateDescription(string memory) internal virtual override pure returns (string memory) {
         return 'This Borrow Ticket NFT was created by the deposit of an NFT into the NFT Loan Faciliator '
                 'contract to serve as collateral for a loan. If the loan is underwritten, funds will be transferred '
                 'to the borrow ticket holder. If the loan is repaid, the NFT collateral is transferred to the borrow '
